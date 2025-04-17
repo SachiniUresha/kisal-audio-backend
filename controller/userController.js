4e33946913d6a4b9faf04277e5fe5619f1d155c6
SachiniUresha <152965808+SachiniUresha@users.noreply.github.com>
2025-04-17 15:29:14 +0530

    Create admin users page ✅

--- controller/userController.js
+++ controller/userController.js
@@ -38,6 +38,13 @@ export function loginUser(req,res){
             if(user==null){
             res.status(404).json({error:"User not found..."})
             }else{
+                if(user.isBlocked){
+                    res.status(403).json({
+                        error:"Your account is blocked. Please contact admin"
+                    });
+                    return;
+                }
+                
                 const isPasswordCorrect = bcrypt.compareSync(data.password, user.password)
 
                 if(isPasswordCorrect){
@@ -82,4 +89,68 @@ export function isItCustomer(req){
     }
 
     return isCustomer;
+}
+
+export async function getAllUsers(req, res) {
+
+    if(isItAdmin(req)){
+        try{
+
+            const users = await User.find();
+            res.json(users);
+
+        }catch(e){
+            res.status(500).json({
+                error: "Failed to get users"
+            })
+        }
+    }else{
+        res.status(403).json({
+            error:"Unauthorized"
+        })
+    }
+    
+}
+
+export async function blockOrUnblockUser(req,res) {
+
+    const email = req.params.email;
+    if(isItAdmin(req)){
+        try{
+            const user = await User.findOne(
+                {
+                    email: email
+                }
+            )
+
+            if(user==null){
+                res.status(404).json({
+                    error:"User not found"
+                });
+                return;
+            }
+             
+            const isBlocked = !user.isBlocked;
+
+            await User.updateOne(
+                {
+                    email:email
+                },
+                {
+                    isBlocked:isBlocked
+                }
+            );
+
+            res.json({
+                message:"User blocked/unblocked successfully"
+            });
+
+        }catch(e){
+            res.status(500).json({error:"Failed to get user"});
+        }
+
+    }else{
+        res.status(403).json({error: "Unauthorized"});
+    }
+    
 }
